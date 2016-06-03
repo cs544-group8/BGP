4bcdb4babdc7698ba8d3ebb8b18de88f52af1544
trhodesjr <trhodes@drexel.edu>
2016-06-02 23:27:43 -0400

    tux version

---
+++ client/tictactoe-linux/src/pdu.cpp
@@ -0,0 +1,66 @@
+//  CS544 - Computer Networks
+//  Group 8
+//  Copyright © 2016 BGP Protocol
+//  Team Members: Ryan Mann, Brian Quinn, TJ Rhodes, Marc Thomson
+//
+//  File name: pdu.cpp
+//  Description: source file for pdu object. Logic for creating / storing a pdu is here
+
+#include "pdu.hpp"
+
+using namespace PDUEnums;
+
+PDU::PDU()
+{
+    m_header.m_version = 1;
+    m_header.m_length = 0;
+    m_header.m_reserved = 0;
+    memset(m_payload.m_data,0,1016);    
+}
+
+PDU::~PDU()
+{
+}
+
+
+void PDU::buildPDU(unsigned int id, int message, string data)
+{
+    this->m_header.m_client_ID = id;
+    unsigned char prep_data[data.length()];
+    data.copy((char*)prep_data, data.length(), 0);
+    
+    switch(message) {
+        case NEWGAMETYPE:
+            this->m_header.m_message_type = NEWGAMETYPE;
+            memcpy(&this->m_payload.m_data, prep_data, data.length());
+            this->m_header.m_length = data.length();
+            break;
+        case MOVE:
+            this->m_header.m_message_type = MOVE;
+            memcpy(&this->m_payload.m_data, prep_data, data.length());
+            this->m_header.m_length = data.length();
+            break;
+        case INVMOVE:
+            this->m_header.m_message_type = INVMOVE;
+            break;
+        case RESET:
+            this->m_header.m_message_type = RESET;
+            break;
+        case RESETACK:
+            this->m_header.m_message_type = RESETACK;
+            break;
+        case RESETNACK:
+            this->m_header.m_message_type = RESETNACK;
+            break;
+        case GAMEEND:
+            this->m_header.m_message_type = GAMEEND;
+            memcpy(&this->m_payload.m_data, prep_data, data.length());
+            this->m_header.m_length = data.length();
+            break;
+        case GAMEENDACK:
+            this->m_header.m_message_type = GAMEENDACK;
+            break;
+        default:
+            this->m_header.m_version = -1; // bad message
+    }
+}
---
+++ client/tictactoe-linux/src/pdu.hpp
@@ -0,0 +1,63 @@
+//  CS544 - Computer Networks
+//  Group 8
+//  Copyright © 2016 BGP Protocol
+//  Team Members: Ryan Mann, Brian Quinn, TJ Rhodes, Marc Thomson
+//
+//  File name: pdu.hpp
+//  Description: header file for pdu object
+
+#ifndef pdu_hpp
+#define pdu_hpp
+
+#include <stdio.h>
+#include <iostream>
+#include <cstring>
+#include <vector>
+using namespace std;
+
+struct pduHeader {
+    unsigned char m_version;
+    unsigned char m_message_type;
+    unsigned char m_length;
+    unsigned char m_reserved;
+    unsigned int m_client_ID;
+};
+
+struct pduPayload {
+    unsigned char m_data[1016];
+};
+
+class PDU
+{
+public:
+    PDU();
+    ~PDU();
+
+public:
+
+    pduHeader m_header;
+    pduPayload m_payload;
+
+    void buildPDU(unsigned int id, int message, string data);
+
+};
+
+namespace PDUEnums
+{
+    enum Messages {
+                    NEWGAMETYPE,
+                    INVGAMETYPE,
+                    CLIENTIDASSIGN,
+                    FOUNDOPP,
+                    PLAYERASSIGN,
+                    MOVE,
+                    INVMOVE,
+                    RESET,
+                    RESETACK,
+                    RESETNACK,
+                    GAMEEND,
+                    GAMEENDACK
+    };
+}
+
+#endif /* pdu_hpp */
